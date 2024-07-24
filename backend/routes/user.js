bd28c0d4bd22f0966a0f5614a66acbcbf333ba5d
Udbhav Patel <86018969+thisisudbhavv@users.noreply.github.com>
2024-07-24 13:34:49 +0530

    zod

--- backend/routes/user.js
+++ backend/routes/user.js
@@ -1,5 +1,9 @@
 const express = require("express");
-
+const zod = require("zod")
 const router = express.Router();
+
+router.get("/signup", (req, res) => {
+    const body = req.body;
+});
 
 module.exports = router;
