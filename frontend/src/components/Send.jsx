c0a5b097c0662bda7b805961611b4280b6dcfc9f
Udbhav Patel <86018969+thisisudbhavv@users.noreply.github.com>
2024-08-05 17:27:16 +0530

    added send functionality and dashboard

--- frontend/src/components/Send.jsx
+++ frontend/src/components/Send.jsx
@@ -1,4 +1,13 @@
+import axios from "axios";
+import { useState } from "react";
+import { useSearchParams } from "react-router-dom";
+
 export const Send = () => {
+  const [amount, setAmount] = useState(0);
+  const [searchParams] = useSearchParams();
+  const id = searchParams.get("id");
+  const name = searchParams.get("name");
+
   return (
     <div class="flex justify-center h-screen bg-gray-100">
       <div className="h-full flex flex-col justify-center">
@@ -9,9 +18,9 @@ export const Send = () => {
           <div class="p-6">
             <div class="flex items-center space-x-4">
               <div class="w-12 h-12 rounded-full bg-green-500 flex items-center justify-center">
-                <span class="text-2xl text-white">A</span>
+                <span class="text-2xl text-white">{name[0].toUpperCase()}</span>
               </div>
-              <h3 class="text-2xl font-semibold">Friend's Name</h3>
+              <h3 class="text-2xl font-semibold">{name}</h3>
             </div>
             <div class="space-y-4">
               <div class="space-y-2">
@@ -22,13 +31,33 @@ export const Send = () => {
                   Amount (in Rs)
                 </label>
                 <input
+                  onChange={(e) => {
+                    setAmount(e.target.value);
+                  }}
                   type="number"
                   class="flex h-10 w-full rounded-md border border-input bg-background px-3 py-2 text-sm"
                   id="amount"
                   placeholder="Enter amount"
                 />
               </div>
-              <button class="justify-center rounded-md text-sm font-medium ring-offset-background transition-colors h-10 px-4 py-2 w-full bg-green-500 text-white">
+              <button
+                onClick={() => {
+                  axios.post(
+                    "https://payment-application-88r5.onrender.com/api/v1/account/transfer",
+                    {
+                      to: id,
+                      amount,
+                    },
+                    {
+                      headers: {
+                        Authorization:
+                          "Bearer " + localStorage.getItem("token"),
+                      },
+                    }
+                  );
+                }}
+                class="justify-center rounded-md text-sm font-medium ring-offset-background transition-colors h-10 px-4 py-2 w-full bg-green-500 text-white"
+              >
                 Initiate Transfer
               </button>
             </div>
--- frontend/src/components/Users.jsx
+++ frontend/src/components/Users.jsx
@@ -1,23 +1,31 @@
 import { useEffect, useState } from "react";
 import { Button } from "./Button";
 import axios from "axios";
+import { useNavigate } from "react-router-dom";
 
 export function Users() {
   const [users, setUsers] = useState([]);
+  const [filter, setFilter] = useState("");
 
   useEffect(() => {
     axios
-      .get("https://payment-application-88r5.onrender.com/api/v1/user/bulk")
+      .get(
+        "https://payment-application-88r5.onrender.com/api/v1/user/bulk?filter=" +
+          filter
+      )
       .then((response) => {
         setUsers(response.data.user);
       });
-  }, []);
+  }, [filter]);
 
   return (
     <div>
       <div className="text-2xl font-bold text-left pt-5 pb-3">Users</div>
       <div className="my-2">
         <input
+          onChange={(e) => {
+            setFilter(e.target.value);
+          }}
           placeholder="Search Users....."
           className="w-full px-2 py-1 border rounded border-slate-300"
         />
@@ -32,6 +40,8 @@ export function Users() {
 }
 
 function User({ user }) {
+  const navigate = useNavigate();
+
   return (
     <div className="flex justify-between border-b-2 py-2">
       <div className="flex">
@@ -47,7 +57,12 @@ function User({ user }) {
         </div>
       </div>
       <div className="flex flex-col justify-center h-full">
-        <Button label={"Send Money"} />
+        <Button
+          onClick={(e) => {
+            navigate("/send?id=" + user._id + "&name=" + user.firstName);
+          }}
+          label={"Send Money"}
+        />
       </div>
     </div>
   );
